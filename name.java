bfe275beab69bb7093d62e465469fa1ed738bd64
ManikandanKarunanithi <manikarnan95@gmail.com>
2016-09-01 17:44:58 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class name
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+int  c=sc.nextInt();
+if(c%2==0)
+System.out.println("even");
+else
+System.out.println("odd");
+}
+}
+
